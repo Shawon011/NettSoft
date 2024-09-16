9dbeb3dea3070cd853ee6b7066bb9d5452deed29
Shawon011 <shawont72@gmail.com>
2024-09-16 14:19:21 +0600

    change

--- script.js
+++ script.js
@@ -2,122 +2,170 @@ const data = [
     {
       src: "/img/GLPI_Logo-color.png",
       title: "GLPI",
-      description: "GLPI is a robust open-source IT asset management and service desk tool designed to help businesses streamline their IT infrastructure. With features like asset tracking, help desk ticketing, and powerful integrations, GLPI provides end-to-end IT management in a centralized platform."
+      description: "GLPI is a robust open-source IT asset management and service desk tool designed to help businesses streamline their IT infrastructure. With features like asset tracking, help desk ticketing, and powerful integrations, GLPI provides end-to-end IT management in a centralized platform.",
+      button: "View More",
+      id: 1
     },
     {
       src: "/img/vaultwarden.png",
       title: "Vaultwarden",
-      description: "Vaultwarden is a lightweight open-source alternative to Bitwarden for securely managing passwords. It provides full control over your data in a self-hosted environment without sacrificing performance."
+      description: "Vaultwarden is a lightweight open-source alternative to Bitwarden for securely managing passwords. It provides full control over your data in a self-hosted environment without sacrificing performance.",
+      button: "View More",
+      id: 2
     },
     {
       src: "/img/logo.svg",
       title: "Chatwoot",
-      description: "Chatwoot is an open-source customer engagement platform that centralizes conversations from email, social media, and website chat into one dashboard."
+      description: "Chatwoot is an open-source customer engagement platform that centralizes conversations from email, social media, and website chat into one dashboard.",
+      button: "View More",
+      id: 3
     },
     {
       src: "/img/plausible-analytics.jpeg",
       title: "Plausible Analytics",
-      description: "Plausible Analytics is a privacy-focused open-source alternative to Google Analytics. It offers essential web analytics insights while adhering to strict privacy regulations like GDPR."
+      description: "Plausible Analytics is a privacy-focused open-source alternative to Google Analytics. It offers essential web analytics insights while adhering to strict privacy regulations like GDPR.",
+      button: "View More",
+      id: 4
     },
     {
       src: "/img/rocket.Chat.png",
       title: "Rocket.Chat",
-      description: "Rocket.Chat is an open-source communication platform built for secure team collaboration across messaging, video calls, and file sharing."
+      description: "Rocket.Chat is an open-source communication platform built for secure team collaboration across messaging, video calls, and file sharing.",
+      button: "View More",
+      id: 5
     },
     {
       src: "/img/Dolibarr_logo.png",
       title: "Dolibarr",
-      description: "Dolibarr is a versatile open-source ERP and CRM solution designed for small and medium-sized businesses. It helps manage various business functions such as accounting, inventory, customer relationships, and more."
+      description: "Dolibarr is a versatile open-source ERP and CRM solution designed for small and medium-sized businesses. It helps manage various business functions such as accounting, inventory, customer relationships, and more.",
+      button: "View More",
+      id: 6
     },
     {
       src: "/img/BigBlueButton_logo.svg.png",
       title: "BigBlueButton",
-      description: "BigBlueButton is an open-source web conferencing tool tailored for online learning. It integrates seamlessly with learning management systems and offers features like real-time video, audio, screen sharing, and breakout rooms."
+      description: "BigBlueButton is an open-source web conferencing tool tailored for online learning. It integrates seamlessly with learning management systems and offers features like real-time video, audio, screen sharing, and breakout rooms.",
+      button: "View More",
+      id: 7
     },
     {
       src: "/img/nextcloudlogo.webp",
       title: "Nextcloud",
-      description: "Nextcloud is an open-source platform that provides secure file sharing, collaboration, and storage services in a self-hosted environment."
+      description: "Nextcloud is an open-source platform that provides secure file sharing, collaboration, and storage services in a self-hosted environment.",
+      button: "View More",
+      id: 8
     },
     {
       src: "/img/EasyAppointments.png",
       title: "EasyAppointments",
-      description: "EasyAppointments is a flexible open-source scheduling solution that helps businesses manage appointments, staff availability, and client bookings."
+      description: "EasyAppointments is a flexible open-source scheduling solution that helps businesses manage appointments, staff availability, and client bookings.",
+      button: "View More",
+      id: 9
     },
     {
       src: "/img/Snipe-IT.png",
       title: "Snipe-IT",
-      description: "Snipe-IT is a powerful open-source asset management system designed to keep track of IT equipment, licenses, and warranties."
+      description: "Snipe-IT is a powerful open-source asset management system designed to keep track of IT equipment, licenses, and warranties.",
+      button: "View More",
+      id: 10
     },
     {
       src: "/img/Mautic_Logo.png",
       title: "Mautic",
-      description: "Mautic is an open-source marketing automation platform that empowers businesses to streamline their digital marketing efforts including email marketing and social media."
+      description: "Mautic is an open-source marketing automation platform that empowers businesses to streamline their digital marketing efforts including email marketing and social media.",
+      button: "View More",
+      id: 11
     },
     {
       src: "/img/OpenProject.jpg",
       title: "OpenProject",
-      description: "OpenProject is a leading open-source project management software designed to handle all aspects of project planning, tracking, and collaboration."
+      description: "OpenProject is a leading open-source project management software designed to handle all aspects of project planning, tracking, and collaboration.",
+      button: "View More",
+      id: 12
     },
     {
       src: "/img/ERPNext.png",
       title: "ERPNext",
-      description: "ERPNext is an open-source ERP solution that covers all aspects of a business, including accounting, inventory, human resources, and manufacturing."
+      description: "ERPNext is an open-source ERP solution that covers all aspects of a business, including accounting, inventory, human resources, and manufacturing.",
+      button: "View More",
+      id: 13
     },
     {
       src: "/img/moodlecover.jpg",
       title: "Moodle",
-      description: "Moodle is a popular open-source learning management system (LMS) that provides educators with the tools to create dynamic, personalized online courses."
+      description: "Moodle is a popular open-source learning management system (LMS) that provides educators with the tools to create dynamic, personalized online courses.",
+      button: "View More",
+      id: 14
     },
     {
       src: "/img/jitsimeet.jpg",
       title: "Jitsi",
-      description: "Jitsi is an open-source video conferencing platform that enables secure, high-quality virtual meetings."
+      description: "Jitsi is an open-source video conferencing platform that enables secure, high-quality virtual meetings.",
+      button: "View More",
+      id: 15
     },
     {
       src: "/img/InvoiceNinja.png",
       title: "Invoice Ninja",
-      description: "Invoice Ninja is an open-source invoicing and billing platform designed for freelancers and small businesses. It allows you to create and send professional invoices, track payments, and manage finances."
+      description: "Invoice Ninja is an open-source invoicing and billing platform designed for freelancers and small businesses. It allows you to create and send professional invoices, track payments, and manage finances.",
+      button: "View More",
+      id: 16
     },
     {
       src: "/img/liTop.svg",
       title: "iTop",
-      description: "iTop is an open-source IT service management (ITSM) solution that helps organizations manage incidents, changes, and service requests."
+      description: "iTop is an open-source IT service management (ITSM) solution that helps organizations manage incidents, changes, and service requests.",
+      button: "View More",
+      id: 17
     },
     {
       src: "/img/Trudesk.png",
       title: "Trudesk",
-      description: "Trudesk is an open-source help desk and ticketing system that allows businesses to manage customer support tickets efficiently."
+      description: "Trudesk is an open-source help desk and ticketing system that allows businesses to manage customer support tickets efficiently.",
+      button: "View More",
+      id: 18
     },
     {
       src: "/img/Trilium.png",
       title: "Trilium",
-      description: "Trilium is a hierarchical note-taking platform designed for structured information management, offering a robust set of features like tagging, linking, and full-text search."
+      description: "Trilium is a hierarchical note-taking platform designed for structured information management, offering a robust set of features like tagging, linking, and full-text search.",
+      button: "View More",
+      id: 19
     },
     {
       src: "/img/akaunting-logo-horizontal.png",
       title: "Akaunting",
-      description: "Akaunting is an open-source accounting software built for small businesses and freelancers. It enables users to manage invoices, track expenses, and monitor finances effortlessly."
+      description: "Akaunting is an open-source accounting software built for small businesses and freelancers. It enables users to manage invoices, track expenses, and monitor finances effortlessly.",
+      button: "View More",
+      id: 20
     },
     {
       src: "/img/listmonk.jpg",
       title: "Listmonk",
-      description: "Listmonk is an open-source self-hosted newsletter and mailing list manager designed to handle mass mailing with high efficiency."
+      description: "Listmonk is an open-source self-hosted newsletter and mailing list manager designed to handle mass mailing with high efficiency.",
+      button: "View More",
+      id: 21
     },
     {
       src: "/img/Authentik.png",
       title: "Authentik",
-      description: "Authentik is an open-source identity provider focused on making authentication and authorization simple and secure."
+      description: "Authentik is an open-source identity provider focused on making authentication and authorization simple and secure.",
+      button: "View More",
+      id: 22
     },
     {
       src: "/img/Plex.jpg",
       title: "Plex",
-      description: "Plex is an open-source media server that allows you to organize and stream your personal collection of movies, TV shows, music, and photos."
+      description: "Plex is an open-source media server that allows you to organize and stream your personal collection of movies, TV shows, music, and photos.",
+      button: "View More",
+      id: 23
     },
     {
       src: "/img/SMTP-Server.png",
       title: "SMTP/E-Mail Server",
-      description: "An open-source SMTP/E-Mail Server enables businesses to manage email communications securely and efficiently, whether for internal communications or email marketing."
+      description: "An open-source SMTP/E-Mail Server enables businesses to manage email communications securely and efficiently, whether for internal communications or email marketing.",
+      button: "View More",
+      id: 24
     }
   ];
   
@@ -146,6 +194,7 @@ data.forEach((item, index) => {
         </div>
         <h2 class="name">${item.title}</h2>
         <p class="desc">${item.description}</p>
+        <button class="viewBtn">${item.button}</button>
       </div>
     `;
     cardContainer.appendChild(card)
@@ -159,6 +208,15 @@ data.forEach((item, index) => {
     }
   });
 
+let viewBtn = document.querySelectorAll(".viewBtn")
+
+viewBtn.forEach((e) => {
+  console.log(e);
+  this.addEventListener('click', () => {
+    console.log("hello");
+    
+  })
+})
 
 // Carousel
 
